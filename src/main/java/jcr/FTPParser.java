e14153746dbd100eb68e6dbd829fe1e77e15b9b1
tbuczko <Tamas.BUCZKO@nng.com>
2015-02-11 09:16:05 +0100

    level3

---
+++ src/main/java/jcr/FTPParser.java
@@ -0,0 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jcr;
+
+import gui.JCRForm;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.jackrabbit.commons.JcrUtils;
+
+/**
+ *
+ * @author tbuczko
+ */
+public class FTPParser {
+
+    FTPClient client;
+    Session session;
+    String jcrServerNodePath;
+    private static Logger logger = Logger.getLogger(JCRForm.class.getName());
+
+    public FTPParser(String host, int port, String user, String pass, String root, Session session) {
+        this.session = session;
+        jcrServerNodePath = "/downloadservers/" + host;
+        try {
+            client = new FTPClient();
+            client.setControlEncoding("UTF-8");
+            client.connect(host, port);
+            client.login(user, pass);
+            parse(root);
+           session.save();
+        } catch (Exception ex) {
+            logger.log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void parse(String workingDirectory) throws RepositoryException, IOException, Exception {
+        Node node = JcrUtils.getOrCreateByPath(jcrServerNodePath + workingDirectory, null,session);
+        String encoded = new String(workingDirectory.getBytes("UTF-8"), "ISO-8859-1");
+        FTPFile[] files = client.listFiles(encoded);
+        logger.log(Level.INFO, "Parse {0} item in directory: {1}", new Object[]{files.length, workingDirectory});
+        for (FTPFile file : files) {
+            if (file.isDirectory()) {
+                parse(workingDirectory + "/" + file.getName());
+            } else {
+                String remote = workingDirectory + "/" + file.getName();
+                if (remote.endsWith(".md5") || remote.endsWith(".md5sum")) {
+                    parseChecksumFile(remote);
+                } else if (remote.endsWith(".tgz.desc") || remote.endsWith(".zip.desc")) {
+                    parseDescriptorFile(remote);
+                } else {
+                    parseFile(remote, file.getSize());
+                }
+            }
+        }
+    }
+
+    private void parseFile(String remote, long size) throws IOException, RepositoryException, NoSuchAlgorithmException {
+        InputStream is = getInputStream(remote);
+        Node node = JcrUtils.getOrCreateByPath(jcrServerNodePath + remote, null,session);
+        node.setProperty("first64Checksum", Utils.getFirst64KBChecksum(is));
+        node.setProperty("size", size);
+        is.close();
+        if (!client.completePendingCommand()) {
+            logger.log(Level.SEVERE, "completePendingCommand Reply: {0}", client.getReplyString());
+        }
+    }
+
+    private void parseDescriptorFile(String remote) throws IOException {
+        InputStream is = getInputStream(remote);
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            String str;
+            while ((str = reader.readLine()) != null) {
+                if (str.startsWith("  {\"filename\":")) {
+//                        str = 
+                }
+            }
+        } finally {
+            try {
+                is.close();
+            } catch (Throwable ignore) {
+            }
+        }
+    }
+
+    private void parseChecksumFile(String remote) throws IOException, RepositoryException {
+        logger.log(Level.INFO, "Read checksum file: {0}", new Object[]{remote});
+        InputStream is = getInputStream(remote);
+        byte[] buffer = new byte[32];
+        int loop = 0;
+        int read = is.read(buffer);
+        logger.log(Level.INFO, "Bytes read from .md5 file: {0}", read);
+        while (read != 32 && loop++ < 3) {
+            is.close();
+            if (!client.completePendingCommand()) {
+                logger.log(Level.SEVERE, "completePendingCommand Reply: {0}", client.getReplyString());
+            }
+            is = getInputStream(remote);
+            read = is.read(buffer);
+            logger.log(Level.INFO, "Bytes read from .md5 file: {0}", read);
+        }
+        if (read != 32) {
+            throw new RuntimeException("Invalid checksum file!");
+        }
+        //
+        String nodeName = jcrServerNodePath + remote;
+        if (nodeName.endsWith(".md5")) {
+            nodeName = nodeName.substring(0, nodeName.lastIndexOf(".")).concat(".fbl");
+        } else {
+            nodeName = nodeName.substring(0, nodeName.lastIndexOf("."));
+        }
+        JcrUtils.getOrCreateByPath(nodeName, null, session).setProperty("md5Checksum", new String(buffer));
+    }
+
+    private InputStream getInputStream(String remote) {
+        InputStream is = null;
+        try {
+            String encoded = new String(remote.getBytes("UTF-8"), "ISO-8859-1");
+            is = client.retrieveFileStream(encoded);
+            logger.log(Level.INFO, "Retrieve File Stream({0}) Reply: {1}", new Object[]{remote, client.getReplyString()});
+            int loop = 0;
+            while (is == null && loop++ < 3) {
+                is = client.retrieveFileStream(remote);
+                logger.log(Level.INFO, "Retry [{0}] Retrieve File Stream({1}) Reply: {2}", new Object[]{loop, remote, client.getReplyString()});
+            }
+            if (is == null) {
+                throw new RuntimeException(client.getReplyString());
+            }
+            return is;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (Exception ex1) {
+                    ex1.printStackTrace();
+                    throw new RuntimeException(ex1);
+                }
+            }
+            throw new RuntimeException(ex);
+        }
+    }
+
+}
---
+++ src/main/java/jcr/FingerprintParser.java
@@ -0,0 +1,181 @@
+package jcr;
+
+import com.naviextras.zippy.apis.services.market.ContentSegment;
+import com.naviextras.zippy.apis.services.market.FileRO;
+import com.naviextras.zippy.apis.services.market.FingerprintRO;
+import gui.JCRForm;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryResult;
+import org.apache.jackrabbit.JcrConstants;
+import org.apache.jackrabbit.commons.JcrUtils;
+
+/**
+ *
+ * @author tbuczko
+ */
+public class FingerprintParser {
+
+    private static final Logger logger = Logger.getLogger(JCRForm.class.getName());
+    private final String repositoryName;
+    private final boolean usePrivateFilestore;
+    private final Session session;
+    private final ParseResult parseResult = new ParseResult();
+    private final String filestoreName;
+    private Node fsRootNode;
+    private Node pfsRootNode;
+    private Node repNode;
+    private final FingerprintRO fingerprint;
+    private final int CHUNK_SIZE = 1024 * 1024;
+
+    public static class ParseResult {
+
+        public int parsedFile = 0;
+        public int newFiles = 0;
+    }
+
+    public FingerprintParser(String repositoryName, String filestoreName, FingerprintRO fingerprint, boolean usePrivateFilestore, Session session) {
+        this.session = session;
+        this.filestoreName = filestoreName;
+        this.fingerprint = fingerprint;
+        this.repositoryName = repositoryName;
+        this.usePrivateFilestore = usePrivateFilestore;
+    }
+
+    public ParseResult parse() throws RepositoryException {
+        fsRootNode = JcrUtils.getOrCreateByPath(filestoreName, null, session);
+        // fsRootNode = JcrUtils.getOrCreateByPath(filestoreName, JcrConstants.NT_FOLDER, session);
+        repNode = JcrUtils.getOrCreateByPath(repositoryName, null, session);
+        // repositoryNode = JcrUtils.getOrCreateByPath(repositoryName, JcrConstants.NT_FOLDER, session);
+        repNode.setProperty("checksum", fingerprint.getChecksum());
+        if (usePrivateFilestore) {
+            pfsRootNode = JcrUtils.getOrCreateByPath(repNode.getParent(), "filestore", false, null, null, false);
+        } else {
+            pfsRootNode = fsRootNode;
+        }
+        for (FileRO file : fingerprint.getFiles()) {
+            parseResult.parsedFile++;
+            Node fNode = JcrUtils.getOrCreateByPath(repNode, file.getPath() + "/" + file.getName(), false, null, null, false);
+            if (!file.isDirectory()) {
+                Node fsNode = getFilestoreNode(fsRootNode, file, false);
+                if (fsNode == null) {
+                    fsNode = getFilestoreNode(pfsRootNode, file, true);
+                }
+                fNode.setProperty(JcrConstants.JCR_CONTENT, fsNode);
+            }
+        }
+        return parseResult;
+    }
+
+    private Node getFilestoreNode(Node fsRootNode, FileRO file, boolean createIfNotExists) throws RepositoryException {
+
+        String fsPath = fsRootNode.getPath();
+        String md5Checksum = file.getChecksum() == null ? null : file.getChecksum().toLowerCase();
+        String first64Checksum = file.getFirst64Checksum() == null ? null : file.getFirst64Checksum().toLowerCase();
+        String size = Long.toString(file.getSize());
+        ContentSegment contentSegment = Utils.getLatestContentSegment(file);
+        String contentId = null;
+        String buildTimestamp = null;
+        if (contentSegment != null) {
+            contentId = Long.toString(contentSegment.getContentId());
+            buildTimestamp = Long.toString(contentSegment.getContentTimestamp().getTime());
+        }
+
+        String expression;
+        if (first64Checksum != null && md5Checksum != null && contentSegment != null) {
+            expression = "SELECT * FROM [nt:base] WHERE ISDESCENDANTNODE([" + fsPath + "]) AND "
+                    + "(([md5Checksum]='" + md5Checksum + "' OR [first64Checksum]='" + first64Checksum + "') OR "
+                    + "([contentId]=CAST(" + contentId + " AS LONG) AND [buildTimestamp]=CAST(" + buildTimestamp + " AS LONG)))"
+                    + " AND [size]=CAST(" + size + " AS LONG)";
+        } else if (first64Checksum != null && contentSegment != null) {
+            expression = "SELECT * FROM [nt:base] WHERE ISDESCENDANTNODE([" + fsPath + "]) AND "
+                    + "([first64Checksum]='" + first64Checksum + "' OR "
+                    + "([contentId]=CAST(" + contentId + " AS LONG) AND [buildTimestamp]=CAST(" + buildTimestamp + " AS LONG)))"
+                    + " AND [size]=CAST(" + size + " AS LONG)";
+        } else if (first64Checksum != null && md5Checksum != null) {
+            expression = "SELECT * FROM [nt:base] WHERE ISDESCENDANTNODE([" + fsPath + "]) AND ([md5Checksum]='" + md5Checksum + "'"
+                    + " OR [first64Checksum]='" + first64Checksum + "') AND [size]=CAST(" + size + " AS LONG))";
+        } else if (md5Checksum != null) {
+            expression = "SELECT * FROM [nt:base] WHERE ISDESCENDANTNODE([" + fsPath + "]) AND "
+                    + "[md5Checksum]='" + md5Checksum + "' AND [size]=CAST(" + size + " AS LONG)";
+        } else if (first64Checksum != null) {
+            expression = "SELECT * FROM [nt:base] WHERE ISDESCENDANTNODE([" + fsPath + "]) AND "
+                    + "[first64Checksum]='" + first64Checksum + "' AND [size]=CAST(" + size + " AS LONG)";
+        } else {
+            throw new RuntimeException("File can not be identified! " + file.getPath() + "/" + file.getName());
+        }
+
+        if (logger.isLoggable(Level.INFO)) {
+            logger.log(Level.INFO, "Search: {0}", expression);
+        }
+
+        QueryResult result = session.getWorkspace().getQueryManager().createQuery(expression, Query.JCR_SQL2).execute();
+        List<Node> nodes = new ArrayList<Node>();
+        for (Node node : JcrUtils.getNodes(result)) {
+            nodes.add(node);
+        }
+
+        if (nodes.size() == 1) {
+            // TODO: Add new properties
+            return nodes.get(0);
+        } else if (nodes.isEmpty()) {
+            if (createIfNotExists) {
+                parseResult.newFiles++;
+                String fsNodeName = UUID.randomUUID().toString();
+                Node fsNode = JcrUtils.getNodeIfExists(fsRootNode, fsNodeName);
+                while (fsNode != null) {
+                    fsNodeName = UUID.randomUUID().toString();
+                    fsNode = JcrUtils.getNodeIfExists(fsRootNode, fsNodeName);
+                }
+                fsNode = fsRootNode.addNode(fsNodeName);
+//            fsNode = fsRootNode.addNode(fsNodeName, JcrConstants.JCR_CONTENT);   
+                if (md5Checksum != null) {
+                    fsNode.setProperty("md5Checksum", md5Checksum);
+                }
+                if (first64Checksum != null) {
+                    fsNode.setProperty("first64Checksum", first64Checksum);
+                }
+                fsNode.setProperty("size", file.getSize());
+                String mime = URLConnection.guessContentTypeFromName(file.getName());
+                if (mime != null) {
+                    fsNode.setProperty(JcrConstants.JCR_MIMETYPE, mime);
+                }
+                if (contentSegment != null) {
+                    fsNode.setProperty("contentId", contentSegment.getContentId());
+                    fsNode.setProperty("buildTimestamp", contentSegment.getContentTimestamp().getTime());
+                }
+                if (file.getCreateTimestamp() != null) {
+                    fsNode.setProperty("createTimestamp", file.getCreateTimestamp().getTime());
+                }
+                if (file.getModifyTimestamp() != null) {
+                    fsNode.setProperty("createTimestamp", file.getModifyTimestamp().getTime());
+                }
+                fsNode.setProperty("complete", false);
+                if (file.getSize() > CHUNK_SIZE) {
+                    fsNode.setProperty("chunked", true);
+                    fsNode.setProperty("complete", false);
+                    int chunkCount = (int) (file.getSize() / CHUNK_SIZE);
+                    for (int i = 0; i < chunkCount; i++) {
+                        Node chunkNode = fsNode.addNode("chunk-" + i);
+                        chunkNode.setProperty("offset", CHUNK_SIZE * i);
+                        chunkNode.setProperty("complete", false);
+                    }
+                }
+                fsNode.addMixin(JcrConstants.MIX_REFERENCEABLE);
+                session.save();
+                return fsNode;
+            }
+            return null;
+        } else {
+            throw new AssertionError("More then one instance found in filestore[" + fsPath + "]!");
+        }
+    }
+}
